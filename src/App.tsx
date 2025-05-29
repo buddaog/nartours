ada694ac53ab5826b4f45672180f484bf6082de5
buddaog <88316545+buddaog@users.noreply.github.com>
2025-05-29 21:23:41 +0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Добро пожаловать в Nartours</h1>
+      <p>Туристическое агентство мечты</p>
+    </div>
+  )
+}
+
+export default App
